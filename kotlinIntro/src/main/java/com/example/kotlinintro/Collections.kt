ba5f4bf5508acc3b1e9b07e7a753c7a6e99a0a42
Raks-Javac <rufaikudus2014@gmail.com>
2023-12-22 12:12:56 +0100

    ref: Collections (mutable list and immutable list)

--- kotlinIntro/src/main/java/com/example/kotlinintro/Collections.kt
+++ kotlinIntro/src/main/java/com/example/kotlinintro/Collections.kt
@@ -5,6 +5,22 @@ fun main(){
 *List
 * ArrayList
 * listOf
-* list Methods
+* list Methods(invoking list methods)
+*
  */
+
+
+    //listOf Example
+//    NOTE: listOf is immutable
+    val listOfExample: List<String> = listOf<String>("Grey","white","Purple")
+    println(listOfExample)
+
+    //mutableListOf Example
+    val mutableListExmple = mutableListOf("lema","jack")
+
+    //invoking list methods
+//    NOTE: not that there are methods available for mutable list(can be modified) and imutable list
+    listOfExample.forEach { println("Names of individuals in the classroom $it") }
+
+
 }
