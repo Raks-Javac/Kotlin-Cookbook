dc55fb01c54e9bd3db20c5f7633b2f07f75779bc
Raks-Javac <rufaikudus2014@gmail.com>
2023-12-24 12:20:11 +0100

    ref: Collections (mutable list and immutable list)

--- kotlinIntro/src/main/java/com/example/kotlinintro/Collections.kt
+++ kotlinIntro/src/main/java/com/example/kotlinintro/Collections.kt
@@ -16,11 +16,24 @@ fun main(){
     println(listOfExample)
 
     //mutableListOf Example
-    val mutableListExmple = mutableListOf("lema","jack")
+    val mutableListExample = mutableListOf("lema","jack")
 
-    //invoking list methods
+    //invoking list methods for immutable list
 //    NOTE: not that there are methods available for mutable list(can be modified) and imutable list
     listOfExample.forEach { println("Names of individuals in the classroom $it") }
+    println("Size ${listOfExample.size}")
+    println("Last ${listOfExample.last()}")
+    println("Binary Search ${listOfExample.binarySearch("grey".uppercase())}")
 
+    //invoking list methods for mutable list
+    //add function
+    mutableListExample.add("david")
+    println(
+        mutableListExample
+    )
+    //remove element and remove  at item index function
+    mutableListExample.removeFirst()
+    mutableListExample.removeAt(1)
+  println(mutableListExample)
 
 }
